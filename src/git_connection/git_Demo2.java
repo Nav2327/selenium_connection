b0f630056cd7de7b858defa660905449bf810c64
naveen <naveennidumolu@gmail.com>
2024-07-04 18:14:44 +0530

    third commit

--- src/git_connection/git_Demo2.java
+++ src/git_connection/git_Demo2.java
@@ -1,5 +1,10 @@
 package git_connection;
 
-public class git_Demo2 {
+public class git_Demo2 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("this is git Demo2 java file");
+	}
 
 }
